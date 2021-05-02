f03912e3ffc8e3cb5844921dd25a491e8aa18c65
judyal-taweel <judybataineh@gmail.com>
2021-05-02 22:25:57 +0300

    lab01

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -36,19 +36,65 @@ Unicorn Tears
 ** What would you like to order? **
 ***********************************
 """)
+
+
+menu = [
+    {
+        "name": "Appetizers",
+        "order": [
+           "Wings",
+            "Cookies",
+            "Spring Rolls",
+        ],
+    },
+        {
+        "name": "Entrees",
+        "order": [
+           "Salmon",
+            "Steak",
+            "Meat Tornado",
+            "A Literal Garden",
+        ],
+    },
+        {
+        "name": "Desserts",
+        "order": [
+           "Ice Cream",
+            "Cake",
+            "Pie",
+        ],
+    },
+        {
+        "name": "Drinks",
+        "order": [
+           "Coffee",
+            "Tea",
+            "Unicorn Tears",
+        ],
+    },
+]
+
+list2 = ["Coffee","Tea","Unicorn Tears","Ice Cream","Cake","Pie","Salmon","Steak","Meat Tornado","A Literal Garden","Wings","Cookies","Spring Rolls"]
+
+
+
 myownmenu = []
 myorder =""
 while myorder != "quit":
     myorder = input('> ')
     print(" ")
-    myownmenu.append(myorder)
-    if myorder != "quit":  
-      if myownmenu.count(myorder) == 1:  
-        print(f"** {myownmenu.count(myorder)} order of {myorder} have been added to your meal ** ")
-      else:
-        print(f"** {myownmenu.count(myorder)} orders of {myorder} have been added to your meal ** ")
+    if myorder in list2:
+       myownmenu.append(myorder)
+       if myorder != "quit":  
+          if myownmenu.count(myorder) == 1:  
+            print(f"** {myownmenu.count(myorder)} order of {myorder} have been added to your meal ** ")
+          else:
+            print(f"** {myownmenu.count(myorder)} orders of {myorder} have been added to your meal ** ")
     
     print(" ")
+
+
+
    
 
 
